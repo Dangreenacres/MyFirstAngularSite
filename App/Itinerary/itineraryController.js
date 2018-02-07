eaddf223d4266ccded24b87d1d50b66d30847c9c
dangreenacres <dangreenacres@gmail.com>
2018-02-07 17:12:22 +0000

    Week 4 complete

--- App/Itinerary/itineraryController.js
+++ App/Itinerary/itineraryController.js
@@ -1,10 +1,27 @@
 ﻿angular.module("itsItinerary").controller("itineraryController", function ($scope) {
-    //$scope.itineraries = ['October Business Development trip 2015', 'November Client-site visit 2015', 'January Scoping visit 2016', 'May Recruitment visit 2016', 'July Conversion visit 2016']
     
     $scope.add = function () {
-        $scope.itineraries.push($scope.itineraryName);
+        var itineraryDetails = {
+            id: $scope.itineraries.length + 1,
+            itiName: $scope.itineraryName,
+            destination: $scope.itineraryDestination,
+            purpose: $scope.itineraryPurpose,
+            startDate: new Date($scope.itineraryStartDate),
+            endDate: new Date($scope.itineraryEndDate)
+        }
+        $scope.itineraries.push(itineraryDetails);
         $scope.isEditing = false;
+
         $scope.itineraryName = "";
+        $scope.itineraryDestination = "";
+        $scope.itineraryPurpose = "";
+        $scope.itineraryStartDate = "";
+        $scope.itineraryEndDate = "";
+    }
+
+    $scope.remove = function (itineraryId) {
+        var itineraryToRemove = $scope.itineraries.indexOf(itineraryId);
+        $scope.itineraries.splice(itineraryToRemove, 1);
     };
 
     $scope.isEditing = false;
@@ -17,47 +34,48 @@
         $scope.isEditing = false;
         $scope.itineraryName = "";
     };
+    $scope.init = function () {
+        $scope.itineraries = [{
+            id: 1,
+            itiName: "October Business Development trip 2015",
+            destination: "Germany",
+            purpose: "Work",
+            startDate: new Date("2015-10-03"),
+            endDate: new Date("2015-10-10")
+        },
+        {
+            id: 2,
+            itiName: "November Client-site visit 2015",
+            destination: "America",
+            purpose: "Work",
+            startDate: new Date("2015-11-05"),
+            endDate: new Date("2015-11-08")
+        },
+        {
+            id: 3,
+            itiName: "January Scoping visit 2016",
+            destination: "China",
+            purpose: "Work",
+            startDate: new Date("2016-01-15"),
+            endDate: new Date("2016-01-23")
+        },
+        {
+            id: 4,
+            itiName: "May Recruitment visit 2016",
+            destination: "India",
+            purpose: "Work",
+            startDate: new Date("2016-05-21"),
+            endDate: new Date("2016-05-30")
+        },
+        {
+            id: 5,
+            itiName: "July Conversion visit 2016",
+            destination: "Germany",
+            purpose: "Work",
+            startDate: new Date("2016-07-08"),
+            endDate: new Date("2015-07-15")
+        }];
+    };
 
-    $scope.itineraries = [{
-        id: 1,
-        itiName: "October Business Development trip 2015",
-        destination: "Germany",
-        purpose: "Work",
-        startDate: new Date("2015-10-03"),
-        endDate: new Date("2015-10-10")
-    },
-    {
-        id: 2,
-        itiName: "November Client-site visit 2015",
-        destination: "America",
-        purpose: "Work",
-        startDate: new Date("2015-11-05"),
-        endDate: new Date("2015-11-08")
-    },
-    {
-        id: 3,
-        itiName: "January Scoping visit 2016",
-        destination: "China",
-        purpose: "Work",
-        startDate: new Date("2016-01-15"),
-        endDate: new Date("2016-01-23")
-    },
-    {
-        id: 4,
-        itiName: "May Recruitment visit 2016",
-        destination: "India",
-        purpose: "Work",
-        startDate: new Date("2016-05-21"),
-        endDate: new Date("2016-05-30")
-    },
-    {
-        id: 5,
-        itiName: "July Conversion visit 2016",
-        destination: "Germany",
-        purpose: "Work",
-        startDate: new Date("2016-07-08"),
-        endDate: new Date("2015-07-15")
-    }];
-
-
+    $scope.init();
 });
