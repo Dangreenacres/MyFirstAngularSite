1fa41fea4fa83469fa42ba6d8f574896b453deec
dangreenacres <dangreenacres@gmail.com>
2018-02-13 09:45:06 +0000

    Tutorial 5 complete but error message not displaying.

--- App/Itinerary/itineraryController.js
+++ App/Itinerary/itineraryController.js
@@ -1,4 +1,4 @@
-﻿angular.module("itsItinerary").controller("itineraryController", function ($scope) {
+﻿angular.module("itsItinerary").controller("itineraryController", function ($scope, $http) {
     
     $scope.add = function () {
         var itineraryDetails = {
@@ -34,47 +34,16 @@
         $scope.isEditing = false;
         $scope.itineraryName = "";
     };
+
+    //Having trouble getting error message to show.
     $scope.init = function () {
-        $scope.itineraries = [{
-            id: 1,
-            itiName: "October Business Development trip 2015",
-            destination: "Germany",
-            purpose: "Work",
-            startDate: new Date("2015-10-03"),
-            endDate: new Date("2015-10-10")
-        },
-        {
-            id: 2,
-            itiName: "November Client-site visit 2015",
-            destination: "America",
-            purpose: "Work",
-            startDate: new Date("2015-11-05"),
-            endDate: new Date("2015-11-08")
-        },
-        {
-            id: 3,
-            itiName: "January Scoping visit 2016",
-            destination: "China",
-            purpose: "Work",
-            startDate: new Date("2016-01-15"),
-            endDate: new Date("2016-01-23")
-        },
-        {
-            id: 4,
-            itiName: "May Recruitment visit 2016",
-            destination: "India",
-            purpose: "Work",
-            startDate: new Date("2016-05-21"),
-            endDate: new Date("2016-05-30")
-        },
-        {
-            id: 5,
-            itiName: "July Conversion visit 2016",
-            destination: "Germany",
-            purpose: "Work",
-            startDate: new Date("2016-07-08"),
-            endDate: new Date("2015-07-15")
-        }];
+        $http.get("http://webteach_net.hallam.sghu.ac.uk/acesjas/api/its")
+            .success(function (response) {
+                $scope.itineraries = response;
+            })
+            .error(function (error) {
+                $scope.errorMessage = error
+            });
     };
 
     $scope.init();
