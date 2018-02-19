09ed1eee0eb32896257c42f152c9cd5d88059a44
dangreenacres <dangreenacres@gmail.com>
2018-02-19 14:09:25 +0000

    Validation tutorial complete with some fixes to show/hide and date format.

--- App/Itinerary/itineraryController.js
+++ App/Itinerary/itineraryController.js
@@ -2,6 +2,41 @@
 
     var itinId;
 
+    //Default View State (On Page Load)
+    $scope.isList = true;
+    $scope.isEditing = false;
+    $scope.isUpdating = false;
+
+    // Add Itinerary
+    $scope.beginEditing = function () {
+        //Reset form values
+        $scope.addForm.$setPristine();
+
+        //View State
+        $scope.isList = false;
+        $scope.isEditing = true;
+        $scope.isUpdating = false;
+    };
+
+    //Cancel Addition
+    $scope.cancelAdd = function () {
+        //View State
+        $scope.isList = true;
+        $scope.isEditing = false;
+        $scope.isUpdating = false;
+
+        //Clear Form
+        $scope.itineraryName = "";
+    };
+
+    //Cancel Edit
+    $scope.cancelUpdate = function () {
+        //View State
+        $scope.isList = true;
+        $scope.isEditing = false;
+        $scope.isUpdating = false;
+    };
+
     $scope.add = function () {
         var itineraryDetails = {
             itiName: $scope.itineraryName,
@@ -14,17 +49,22 @@
         $http.post("http://webteach_net.hallam.shu.ac.uk/acesjas/api/its", itineraryDetails)
             .success(function () {
                 $scope.init();
+
+                //View State
+                $scope.isList = true;
+                $scope.isEditing = false;
+                $scope.isUpdating = false;
+
+                //Clear Form
+                $scope.itineraryName = "";
+                $scope.itineraryDestination = "";
+                $scope.itineraryPurpose = "";
+                $scope.itineraryStartDate = "";
+                $scope.itineraryEndDate = "";
             })
             .error(function (error) {
                 $scope.errorMessage = error;
             });
-        $scope.isEditing = false;
-
-        $scope.itineraryName = "";
-        $scope.itineraryDestination = "";
-        $scope.itineraryPurpose = "";
-        $scope.itineraryStartDate = "";
-        $scope.itineraryEndDate = "";
     }
     
     $scope.remove = function (itineraryId) {
@@ -37,25 +77,21 @@
             });
     };
 
-    // Set default on page load
-    $scope.isEditing = false;
-    $scope.isUpdating = false;
-
-    // Add Itinerary
-    $scope.beginEditing = function () {
-        $scope.isEditing = true;
-    };
-
     // Edit Itinerary
     $scope.displayEditItinerary = function (itineraryId) {
+
+        //View State
+        $scope.isList = false;
+        $scope.isEditing = false;
         $scope.isUpdating = true;
+
         $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/its/" + itineraryId)
             .success(function (response) {
                 $scope.editItineraryName = response.itiName;
                 $scope.editItineraryDestination = response.destination;
                 $scope.editItineraryPurpose = response.purpose;
-                $scope.editItineraryStartDate = response.startDate;
-                $scope.editItineraryEndDate = response.endDate;
+                $scope.editItineraryStartDate = new Date(response.startDate);
+                $scope.editItineraryEndDate = new Date(response.endDate);
 
                 itinId = response.Id;
             })
@@ -70,13 +106,18 @@
             itiName: $scope.editItineraryName,
             destination: $scope.editItineraryDestination,
             purpose: $scope.editItineraryPurpose,
-            startDate: new Date($scope.itineraryStartDate, "dd/MM/yyyy"),
-            endDate: new Date($scope.itineraryEndDate, "dd/MM/yyyy")
+            startDate: new Date($scope.editItineraryStartDate),
+            endDate: new Date($scope.editItineraryEndDate)
         }
 
         $http.put("http://webteach_net.hallam.shu.ac.uk/acesjas/api/its", itineraryDetails)
             .success(function () {
+
+                //View State
+                $scope.isList = true;
+                $scope.isEditing = false;
                 $scope.isUpdating = false;
+
                 $scope.init();
             })
             .error(function (error) {
@@ -84,15 +125,6 @@
             });
 
     }
-
-    $scope.cancelAddition = function () {
-        $scope.isEditing = false;
-        $scope.itineraryName = "";
-    };
-
-    $scope.cancelEditing = function () {
-        $scope.isUpdating = false;
-    };
     
     $scope.init = function () {
         $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/its")
